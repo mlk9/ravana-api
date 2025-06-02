87d5c24c14082ccbb132c0d494dd9d8313b0d768
Mohammad Maleki <malekii24@outlook.com>
2025-06-02 13:34:24 +0330

    [fix] time

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -251,7 +251,7 @@ class ArticleTest extends TestCase
             ->assertStatus(201)
             ->assertJsonStructure(['data']);
 
-        $data['published_at'] = now()->format('Y-m-d\TH:i:s.u\Z');
+        $data['published_at'] = now()->format('Y-m-d H:i:s');
 
         $this->assertDatabaseHas(Article::class, $data);
 
